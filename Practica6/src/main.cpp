ec38e4a92d05ad26f0236e4ad8783cc7016f1c51
AlbertBatllo <100155905+AlbertBatllo@users.noreply.github.com>
2022-06-04 13:56:32 +0200

    Update main.cpp

--- Practica6/src/main.cpp
+++ Practica6/src/main.cpp
@@ -39,7 +39,7 @@ else
         Serial.println("archivo.txt doesn't exist");
 }
   myFile = SD.open("/archivo.txt", FILE_WRITE);//abrimos  el archivo 
-  myFile.println("Hola mundo");
+  myFile.println("algo");
   myFile.close();
   myFile=SD.open("/archivo.txt");
   if (myFile) {
